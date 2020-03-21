549eac4f04c03d29f92fde3e5ab7dc8d97c32cbc
ICHeadache <55867190+ICHeadache@users.noreply.github.com>
2020-03-20 23:13:02 -0700

    Found Ham and Spam Count

--- HW2_Question3.py
+++ HW2_Question3.py
@@ -1,44 +1,61 @@
 import numpy as np
 import pandas as pd
 import string
+from sklearn.feature_extraction.text import CountVectorizer
+
+from nltk.corpus import stopwords
 
 # Step 1
-message = [line.rstrip() for line in open("SMSSpamCollection")]
+print("-------------------Message DataFrame----------------------")
+message = pd.read_csv('SMSSpamCollection',sep='\t',names=["labels","message"])
 
-# for message_no,message in enumerate(message[:10]):
+# for message_no,message in enumerate(message[::]):
 #     print(message_no,message)
 #     print('\n')
 
-message = pd.read_csv("SMSSpamCollection", sep = '\t', names = ["labels", "message"])
-# print(message.head())
-
 test_data = []
 training_data = []
-print("Length of the message: ",len(message))
-for i in range(11):
+
+print("Length of the message: ", len(message))
+
+for i in range(len(message)):
     if(i % 10 == 0):
         test_data.append(message.iloc[i])
     else:
         training_data.append(message.iloc[i])
-print("-------------------Message DataFrame----------------------")
+
 # print(message)
 
 pd_test_data = pd.DataFrame(test_data)
 pd_training_data = pd.DataFrame(training_data)
 
-print("-------------------Test Data DataFrame:------------------- \n", pd_test_data)
-print("-------------------Training Data DataFrame:--------------- \n", pd_training_data)
+# print(len(test_data), "DATA")
+# print(len(pd_training_data), "TRAINING")
 
+# print("-------------------Test Data DataFrame:------------------- \n", pd_test_data)
+# print("-------------------Training Data DataFrame:--------------- \n", pd_training_data)
 
-# Step 2 Remove Punctuation
-print(pd_training_data.head(),"\n\n\n")
-for i in range(9):
+# Step 2 
+
+# Remove Punctuation for Training Set
+for i in range(len(pd_training_data)):
     mess = str(pd_training_data.iloc[i]['message'])
     nopunc=[char for char in mess if char not in string.punctuation]
     nopunc=''.join(nopunc)
     pd_training_data.iloc[i]['message'] = nopunc
 
-print(pd_training_data.head())
+# Remove Punctuation for Testing Set
+for i in range(len(pd_test_data)):
+    mess = str(pd_test_data.iloc[i]['message'])
+    nopunc=[char for char in mess if char not in string.punctuation]
+    nopunc=''.join(nopunc)
+    pd_test_data.iloc[i]['message'] = nopunc
+
+# HAM and SPAM count
+print("HAM COUNT: ", pd_training_data.groupby('labels').get_group('ham').count().values[0])
+print("SPAM COUNT: ", pd_training_data.groupby('labels').get_group('spam').count().values[0])
+
+
 
 
 
